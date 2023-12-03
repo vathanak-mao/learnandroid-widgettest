7ee93d2cb79aac7ad85f22a27486ec95a7f0895e
Vathanak Mao <vathanakmao@gmail.com>
2023-12-03 15:31:54 +0700

    Added ImageButtonTestActivity

---
+++ app/src/main/java/com/hfad/widgettest/ImageButtonTestActivity.java
@@ -0,0 +1,15 @@
+package com.hfad.widgettest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageButtonTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_image_button_test);
+    }
+}
