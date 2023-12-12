970d77f9a5c0dd376287b04741024de6e5edee42
Vathanak Mao <vathanakmao@gmail.com>
2023-12-12 17:06:16 +0700

    Added LayoutWidgetExampleActivity

---
+++ app/src/main/java/com/hfad/widgettest/LayoutWidgetExampleActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.widgettest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutWidgetExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_widget_example);
+    }
+}
